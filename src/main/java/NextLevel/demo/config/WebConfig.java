d1c6f535d6460c290b7d6f5aa33e6606056ddbd5
kwonhee1 <126564538+kwonhee1@users.noreply.github.com>
2025-04-30 11:40:24 +0900

    allow cors https

--- src/main/java/NextLevel/demo/config/WebConfig.java
+++ src/main/java/NextLevel/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해
-                    .allowedOrigins("http://localhost:3000") // 프론트 주소
+                    .allowedOrigins("http://localhost:3000", "https://localhost:3000") // 프론트 주소
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .exposedHeaders("access")
