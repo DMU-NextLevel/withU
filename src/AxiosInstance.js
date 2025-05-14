0b0d7ba148cab951efe1fb505fb7be72b4ca908a
SeongHwan <epaktldk0203@gmail.com>
2025-05-14 15:52:49 +0900

    feat: 로컬api 설정 추가

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -4,3 +4,8 @@ export const api = axios.create({
     baseURL: 'https://api.nextlevel.r-e.kr/',
     withCredentials: true
 })
+
+export const testApi = axios.create({
+    baseURL:'http://localhost:8080/',
+    withCredentials: true
+})
