ffd94e6eb153b7aa83460cd9d2d46a0cb0d0e841
kwonhee1 <lkh8033@gmail.com>
2025-05-28 15:14:14 +0900

    feat : defaul tag data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into tag values(1, "테크 가전");
+insert into tag values(2, "라이프 스타일");
+insert into tag values(3, "패션 잡화");
+insert into tag values(4, "뷰티 헬스");
+insert into tag values(5, "취미");
+insert into tag values(6, "게임");
