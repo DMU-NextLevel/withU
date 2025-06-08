a0b6bbc812e1773994da41fe4097e13fa4066d65
kwonhee1 <lkh8033@gmail.com>
2025-06-09 01:52:12 +0900

    edit notice return dto :: add src img url

--- src/main/java/NextLevel/demo/notice/dto/ResponseNoticeDto.java
+++ src/main/java/NextLevel/demo/notice/dto/ResponseNoticeDto.java
@@ -5,6 +5,8 @@ import NextLevel.demo.notice.entity.NoticeEntity;
 import NextLevel.demo.notice.entity.NoticeImgEntity;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +14,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter @Getter
 public class ResponseNoticeDto {
+
+    private static Pattern compiledImgPattern = Pattern.compile("src=");
 
     private Long id;
 
@@ -25,10 +29,24 @@ public class ResponseNoticeDto {
         ResponseNoticeDto dto = new ResponseNoticeDto();
         dto.id = entity.getId();
         dto.title = entity.getTitle();
-        dto.content = entity.getContent();
+        dto.content = makeContent(entity.getContent(), entity.getImgs().stream().map(NoticeImgEntity::getImg).map(ImgEntity::getUri).toList());
         dto.createdAt = entity.getCreatedAt();
         dto.imgs = entity.getImgs().stream().map(NoticeImgEntity::getImg).map(ImgEntity::getUri).toList();
         return dto;
+    }
+
+    private static String makeContent(String content, List<String> imgs) {
+        Matcher matcher = compiledImgPattern.matcher(content);
+        StringBuffer result = new StringBuffer();
+        int index = 0;
+
+        while (matcher.find() && index < imgs.size()) {
+            String replacement = "src=\"https://api.nextlevel.r-e.kr/img/" + imgs.get(index++) + "\"";
+            matcher.appendReplacement(result, Matcher.quoteReplacement(replacement));
+        }
+        matcher.appendTail(result);
+
+        return result.toString();
     }
 
 }
